cda97a2f12cde8badb1b075df9c6049c507d1607
Sumit kumar <Sumit@frontendzip.com>
2024-05-28 12:39:16 +0530

    complete part of logo

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import './Navbar.css'
+import logo from './myntra.svg'
+function Navbar() {
+    return (
+        <nav>
+            <img src={logo} alt="" />
+            <div></div>
+            <div></div>
+            <div></div>
+        </nav>
+    )
+}
+
+export default Navbar
