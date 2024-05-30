80969c65d746c6c4b374445fd1fbe34da92b432e
Sumit kumar <Sumit@frontendzip.com>
2024-05-30 13:45:24 +0530

    create a Productslider Structure

--- src/Components/ProductSlider/ProductSlider.jsx
+++ src/Components/ProductSlider/ProductSlider.jsx
@@ -1,8 +1,29 @@
- 
+import { image1, image2, image3 } from './ProductSliderImage/ProductSliderImage'
+import './ProductSlider.css'
 function ProductSlider() {
-  return (
-    <div>ProductSlider</div>
-  )
+
+    return (
+        <div className='productSlider'>
+            <div className="imagesCard">
+                <div className="cards">
+                    {
+                        image1.map((image, index) => (<img src={image} key={index} />))
+                    }
+                </div>
+                <div className="cards">
+                    {
+                        image2.map((image, index) => (<img src={image} key={index} />))
+                    }
+                </div>
+                <div className="cards">
+                    {
+                        image3.map((image, index) => (<img src={image} key={index} />))
+                    }
+                </div>
+            </div>
+            <div className="buttons"></div>
+        </div>
+    )
 }
 
 export default ProductSlider
