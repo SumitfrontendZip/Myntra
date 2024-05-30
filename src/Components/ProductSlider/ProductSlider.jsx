770937496c5c85967ac4fa4fc936e13c6c85c4ac
Sumit kumar <Sumit@frontendzip.com>
2024-05-30 15:05:25 +0530

    connet to button in Product slider

--- src/Components/ProductSlider/ProductSlider.jsx
+++ src/Components/ProductSlider/ProductSlider.jsx
@@ -1,31 +1,28 @@
 import { image1, image2, image3 } from './ProductSliderImage/ProductSliderImage'
 import './ProductSlider.css'
+import { useState } from 'react'
 
 function ProductSlider() {
+    const [slider, setSlider] = useState(0)
     const images = [image1, image2, image3]
+
+    const handleSliderChange = (direction) => {
+        setSlider(direction);
+    };
+
     return (
         <div className='productSlider'>
             <div className="imagesCard">
                 <div className="cards">
-                    {
-                        image1.map((image, index) => (<img src={image} key={index} />))
-                    }
-                </div>
-                <div className="cards">
-                    {
-                        image1.map((image, index) => (<img src={image} key={index} />))
-                    }
-                </div>
-                <div className="cards">
-                    {
-                        image1.map((image, index) => (<img src={image} key={index} />))
-                    }
+                    {images[slider].map((image, index) => (
+                        <img src={image} key={index} />
+                    ))}
                 </div>
             </div>
             <div className="buttons">
-                {
-                    images.map((_, index) => (<span key={index}></span>))
-                }
+                {images.map((_, index) => (
+                    <span key={index} onClick={() => handleSliderChange(index)} className={index === slider ? 'show' : 'show-hidden'}></span>
+                ))}
             </div>
         </div>
     )
