49cb7f68f3afd781679714d7ab6254082c9f6678
Sumit kumar <Sumit@frontendzip.com>
2024-05-31 09:13:38 +0530

    add media query for Navbar Responsive

--- src/Components/Header/Navbar/Navbar.jsx
+++ src/Components/Header/Navbar/Navbar.jsx
@@ -3,9 +3,11 @@ import Mainmenu from '../mainMenu/Mainmenu'
 import './Navbar.css'
 import NavProfile from '../NavProfile/NavProfile'
 import logo from './myntra.svg'
+import Icon from './menu.png'
 function Navbar() {
     return (
         <nav>
+            <img src={Icon} alt='' className='burggerIcon'/>
             <img src={logo} alt="" />
             <Mainmenu />
             <SearchBar/>
