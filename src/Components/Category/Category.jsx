75fec616bedd7b23b53cec4da9bf2b9488ba702d
Sumit kumar <Sumit@frontendzip.com>
2024-05-31 12:25:23 +0530

    complete Shop of Category Component

--- src/Components/Category/Category.jsx
+++ src/Components/Category/Category.jsx
@@ -1,9 +1,22 @@
 import shopCategory from './shopCategory.png'
 import './Category.css'
+import Images from '../CategoryImages/CategoryImages'
 function Category() {
+    console.log(Images);
     return (
         <div>
-            <img src={shopCategory} alt="" className='shopCategory'/>
+            <div className="shopCategory">
+                <img src={shopCategory} alt="" />
+            </div>
+            <div className="categoryCards">
+                {
+                    Images.map((image,index)=>{
+                        return(
+                            <img src={image} alt={image} key={index} />
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
--- src/Components/CategoryImages/CategoryImages.js
+++ src/Components/CategoryImages/CategoryImages.js
@@ -32,8 +32,8 @@ import img31 from './Image_ (31).png';
 import img32 from './Image_ (32).png';
 import img33 from './Image_.png';
 
-const images = [
+export default  [
     img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15, img16, img17, img18, img19, img20, img21, img22, img23, img24, img25, img26, img27, img28, img29, img30, img31, img32, img33
 ];
 
-export default images;
+  
