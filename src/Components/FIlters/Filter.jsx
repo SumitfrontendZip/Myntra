67824980792a2bc9223bd343fe2ea9e0ce472f27
Sumit kumar <Sumit@frontendzip.com>
2024-06-01 13:47:10 +0530

    update data

--- src/Components/FIlters/Filter.jsx
+++ src/Components/FIlters/Filter.jsx
@@ -4,17 +4,8 @@ function Filter() {
     return (
         <div className="filter">
             <span>Filters</span>
-            <div className="Range">
-                <label htmlFor="Price">Price</label>
-                <input type="range" name="" id="" />
-            </div>
-            <div className="newItem">
-                <input type="checkbox" name="" id="" />
-                <label htmlFor="newItem">New Item</label>
-            </div>
-            <div className="Range">
-                <label htmlFor="Price">Rating</label>
-                <input type="range" name="" id="" />
+            <div className="filterContainer">
+                
             </div>
         </div>
     )
