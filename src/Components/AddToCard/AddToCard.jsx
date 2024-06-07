97bec27bf1bc71de32cd43de15a1ef24aaa28224
Sumit kumar <Sumit@frontendzip.com>
2024-06-07 12:16:54 +0530

    update Add to Card

--- src/Components/AddToCard/AddToCard.jsx
+++ src/Components/AddToCard/AddToCard.jsx
@@ -1,52 +1,31 @@
 import './AddToCard.css'
+import { AddCard } from './AddCard'
+import discountIcon from './discount.png'
+
 export const AddToCard = () => {
     return (
         <div className='addToCard'>
             <section>
                 <div className="deliverySection">
-                    <span>Deliver to : <span className='deliveryCode'>{ 244001}</span></span>
+                    <span>Deliver to : <span className='deliveryCode'>{244001}</span></span>
                     <button>Change Address</button>
                 </div>
                 <div className="offerSection">
-                    <span><img src="" alt="" /><span>Available Offers</span></span>
+                    <span><img src={discountIcon} alt="" /><span>Available Offers</span></span>
                     <span>10% Instant Discount on SBI Credit Card and Credit Card EMI transactions on min. spend of ₹3,500. TCA</span>
                 </div>
                 <div className="removeSection">
                     <span>
                         <input type="checkbox" />
-                        <label htmlFor="selected">{ }/{ } ITEMS SELECTED</label>
+                        <label htmlFor="selected">{1}/{1} ITEMS SELECTED</label>
                     </span>
                     <span>
-                        <button className="remove">REMOVE</button>
-                        <button className="moveToWishlist">MOVE TO WISHLIST</button>
+                        <span className='remove'>REMOVE</span>
+                        <span className='remove'>MOVE TO WISHLIST</span>
                     </span>
                 </div>
-                <div className="placeOrderCard">
-                    <input type="checkbox" />
-                    <div className="cardContent">
-                        <span className="name">The Bear</span>
-                        <span className="brandName">Brandf</span>
-                        <span className="color">Red</span>
-                        <button className="size">
-                            SIZE : { }
-                        </button>
-                        <button className="size">
-                            QTY : { }
-                        </button>
-                        <div className="priceSection">
-                            <span className="price">₹{}</span>
-                            <span className="discountedPrice">₹{}</span>
-                            <span className="totalOff">%</span>
-                        </div>
-                        <span>
-                            <img src="" alt="" />
-                            <span><span>7 Days</span> return Available</span>
-                        </span>
-                        <span>
-                            <img src="" alt="" />
-                            <span>Delivery between {}</span>
-                        </span>
-                    </div>
+                <div className="wrapCards">
+                    <AddCard />
                 </div>
             </section>
             <section>
